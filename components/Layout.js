a9503b4725332afbd5aeacb9eb0cbcd60ba2ca2b
sthobis <s.thobi.sinaga@gmail.com>
2018-09-27 15:45:37 +0700

    feat: add progress/loading bar for page transition

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,6 @@
-import { css } from "emotion";
+import { css, injectGlobal } from "emotion";
+import Router from "next/router";
+import NProgress from "nprogress";
 import PropTypes from "prop-types";
 import React, { PureComponent } from "react";
 
@@ -34,6 +36,37 @@ const styles = {
     padding: "0 30px"
   })
 };
+
+injectGlobal`
+  #nprogress {
+    pointer-events: none;
+
+    .bar {
+      background: turquoise;
+      position: fixed;
+      z-index: 1031;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+    }
+
+    .peg {
+      display: block;
+      position: absolute;
+      right: 0px;
+      width: 100px;
+      height: 100%;
+      box-shadow: 0 0 10px turquoise, 0 0 5px turquoise;
+      opacity: 1;
+      transform: rotate(3deg) translate(0px, -4px);
+    }
+  }
+`;
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 Layout.defaultProps = {
   title: "Influence"
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Layout from "../components/Layout";
+import { setUser } from "../store";
 import parseUserFromCookie from "../utils/parseUserFromCookie";
 
 class Home extends Component {
