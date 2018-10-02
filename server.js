db042ec97aefeb136665702fbf809c76e6e18f07
sthobis <s.thobi.sinaga@gmail.com>
2018-10-02 23:43:45 +0700

    chore: change application port

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
+    server.listen(process.env.PORT || 3100, err => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+      console.log(`> Ready on http://localhost:${process.env.PORT || 3100}`);
     });
   })
   .catch(ex => {
